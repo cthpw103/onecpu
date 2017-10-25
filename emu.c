69c249edf9154ddb596ecd264bb8057d65b002a2
cth103 <cthdev@email.com>
2017-10-25 13:22:06 +0100

    moo!

---
+++ emu.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+//lazy so skidding from urban muellers brainfuck implementation
+int  b;
+char a[255]; //lol funny
+
+
+void doit(char *c)
+{
+	while( *c ) {
+		switch(o=1,*c++) {
+		case 'mv plus': a[b]++;     break;
+    case 'mv minus': a[b]--;     break;
+    case 'mv plus2': b++;     break;
+    case 'mv minus2': b--;     break;
+    case 'mv pr': putchar(a[p]); fflush(stdout); break;
+	}
+}
+
+int main(int argc,char *argv[])
+{
+	FILE *z;
+
+	q=argc;
+
+	if(z=fopen(argv[1],"r")) {
+		while( (b=getc(z))>0 )
+			*s++=b;
+		*s=0;
+		interpret(f);
+	}
+}
